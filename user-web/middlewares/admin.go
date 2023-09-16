4dc61c4e63398bb8a47e39d06c8739a9c9e41edd
xiangnan0811 <weibo0718@126.com>
2023-09-16 16:10:31 +0800

    add admin authentication handler

---
+++ user-web/middlewares/admin.go
@@ -0,0 +1,23 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/xiangnan0811/mxshop-api/user-web/models"
+)
+
+func IsAdminAuth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		claims, _ := c.Get("claims")
+		currentUser := claims.(*models.CustomClaims)
+
+		if currentUser.AuthorityId == 2 {
+			c.JSON(403, gin.H{
+				"msg": "无权限",
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
--- user-web/router/user.go
+++ user-web/router/user.go
@@ -12,7 +12,7 @@ func InitUserRouter(Router *gin.RouterGroup) {
 	UserRouter := Router.Group("user")
 	zap.S().Infow("配置用户相关的url")
 	{
-		UserRouter.GET("list", middlewares.JWTAuth(), api.GetUserList) // 用户列表页
-		UserRouter.POST("login", api.PassWordLogin)                    // 用户登录
+		UserRouter.GET("list", middlewares.JWTAuth(), middlewares.IsAdminAuth(), api.GetUserList) // 用户列表页
+		UserRouter.POST("login", api.PassWordLogin)                                               // 用户登录
 	}
 }
