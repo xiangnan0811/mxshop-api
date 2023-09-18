a2d2aa87afd2dd3cfbc74671f9d471f70dd43cd0
xiangnan0811 <weibo0718@126.com>
2023-09-18 15:43:55 +0800

    add health api

--- user-web/initialize/router.go
+++ user-web/initialize/router.go
@@ -1,6 +1,8 @@
 package initialize
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 
 	"github.com/xiangnan0811/mxshop-api/user-web/middlewares"
@@ -13,6 +15,13 @@ func Routers() *gin.Engine {
 	// 跨域问题
 	Router.Use(middlewares.Cors())
 
+	// 健康检查
+	Router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"code": http.StatusOK,
+			"msg":  "ok",
+		})
+	})
 	ApiRouter := Router.Group("/u/v1")
 	router.InitUserRouter(ApiRouter) // 注册用户路由
 	router.InitBaseRouter(ApiRouter) // 注册基础路由
